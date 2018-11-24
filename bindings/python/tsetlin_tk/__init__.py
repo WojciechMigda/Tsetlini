cfcbcdef68793586bdda70e675f35a2cdd5a9e8b
Wojciech Migda <wojtek.golf@interia.pl>
2018-11-24 23:39:36 +0100

    Add .gitignore in bindings/python

--- bindings/python/tsetlin_tk/__init__.py
+++ bindings/python/tsetlin_tk/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 #
-"""Init for mylibrary."""
+"""Init for tsetlin_tk."""
 
 from __future__ import absolute_import
 
 # This is extracted automatically by the top-level setup.py.
-__version__ = '0.1.5'
+__version__ = '0.0.1'
 
 # add any imports here, if you wish to bring things into the library's top-level namespace when the library is imported.
 
