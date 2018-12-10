88eedfc71b6833801a38230b2df4b7e7a32e3ab2
Wojciech Migda <wojtek.golf@interia.pl>
2018-12-10 22:26:03 +0100

    Implement usable cythonized Either

--- bindings/python/tsetlin_tk/either.pxd
+++ bindings/python/tsetlin_tk/either.pxd
@@ -2,13 +2,12 @@
 # distutils: language = c++
 
 from libcpp cimport bool
-from cython cimport typeof
 
 cdef extern from "neither/either.hpp" namespace "neither":
-    cdef cppclass Either[L, R, LL=*, RR=*]:
+    cdef cppclass Either[L, R]:
         union u:
             L leftValue
             R rightValue
         const bool isLeft
-        Either[L, RR] rightMap[F](const F & fn)
-        Either[LL, R] leftMap[F](const F & fn)
+        Either[L, R] rightMap[FR](const FR & fn)
+        Either[L, R] leftMap[FL](const FL & fn)
--- bindings/python/tsetlin_tk/libtsetlin.pyx
+++ bindings/python/tsetlin_tk/libtsetlin.pyx
@@ -12,7 +12,7 @@ cimport libtsetlin
 #from either cimport Either
 
 from libcpp.memory cimport unique_ptr
-#from libcpp.string cimport string
+from libcpp.string cimport string
 
 from tsetlin_status_code cimport status_message_t
 from tsetlin_classifier_state cimport ClassifierState
@@ -20,15 +20,18 @@ from tsetlin_classifier_state_maker cimport make_classifier_state_ptr
 from move_unique cimport move_unique
 
 
-cdef unique_ptr[ClassifierState] right_fn(unique_ptr[ClassifierState] && state_p):
+cdef unique_ptr[ClassifierState] classifier_fn_right(unique_ptr[ClassifierState] && state_p):
     return move_unique(state_p)
 
-cdef status_message_t left_fn(status_message_t && msg):
+
+cdef status_message_t classifier_fn_left(status_message_t && msg):
     return msg
 
 
 def fit_classifier(js_params):
 
-    make_classifier_state_ptr(js_params)#.leftMap(left_fn).rightMap(right_fn)
+    cdef string jss_params = js_params
+
+    make_classifier_state_ptr(jss_params).leftMap(classifier_fn_left).rightMap(classifier_fn_right)
 
     return
