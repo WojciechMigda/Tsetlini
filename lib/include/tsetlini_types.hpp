a0193e15ca001fc17690abe3e841e7d959e124c4
Wojciech Migda <wojtek.golf@interia.pl>
2020-07-21 21:27:04 +0200

    Add new Tsetlini types

--- lib/include/tsetlini_types.hpp
+++ lib/include/tsetlini_types.hpp
@@ -3,6 +3,8 @@
 #include "mt.hpp"
 #include "aligned_allocator.hpp"
 #include "basic_numeric_matrix.hpp"
+#include "basic_bit_vector.hpp"
+#include "basic_bit_matrix.hpp"
 
 #include <vector>
 #include <random>
@@ -32,10 +34,24 @@ using aligned_vector_int16 = aligned_vector<std::int16_t>;
 using aligned_vector_int8 = aligned_vector<std::int8_t>;
 
 template<typename T>
-using numeric_matrix = basic_numeric_matrix<T, 32>;
+using numeric_matrix = basic_numeric_matrix<T, alignment>;
 using numeric_matrix_int32 = numeric_matrix<std::int32_t>;
 using numeric_matrix_int16 = numeric_matrix<std::int16_t>;
 using numeric_matrix_int8 = numeric_matrix<std::int8_t>;
+
+template<typename T>
+using bit_matrix = basic_bit_matrix<T, alignment>;
+using bit_matrix_uint64 = bit_matrix<std::uint64_t>;
+using bit_matrix_uint32 = bit_matrix<std::uint32_t>;
+using bit_matrix_uint16 = bit_matrix<std::uint16_t>;
+using bit_matrix_uint8 = bit_matrix<std::uint8_t>;
+
+template<typename T>
+using bit_vector = basic_bit_vector<T, alignment>;
+using bit_vector_uint64 = bit_vector<std::uint64_t>;
+using bit_vector_uint32 = bit_vector<std::uint32_t>;
+using bit_vector_uint16 = bit_vector<std::uint16_t>;
+using bit_vector_uint8 = bit_vector<std::uint8_t>;
 
 using label_type = int;
 using label_vector_type = std::vector<label_type>;
