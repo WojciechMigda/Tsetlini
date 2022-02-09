9cc5e1e769406ec1201a5cc45f7dcf94ba9f47f1
Wojciech Migda <wojtek.golf@interia.pl>
2022-02-09 17:32:04 +0100

    Migrate Regressor fit() unit tests to boost.ut
    (#116)

---
+++ lib/utests/src/test_regressor_classic_fit.cpp
@@ -0,0 +1,186 @@
+#include "tsetlini.hpp"
+#include "tsetlini_types.hpp"
+
+#include "boost/ut.hpp"
+
+#include <cstdlib>
+#include <vector>
+
+
+using namespace boost::ut;
+
+
+suite TestRegressorClassicFit = []
+{
+
+
+"RegressorClassic::fit rejects empty input X"_test = []
+{
+    Tsetlini::make_regressor_classic("{}")
+        .rightMap(
+        [](auto && reg)
+        {
+            std::vector<Tsetlini::aligned_vector_char> X;
+            Tsetlini::response_vector_type y{1, 0, 1, 0};
+
+            auto const rv = reg.fit(X, y);
+
+            expect(that % Tsetlini::StatusCode::S_VALUE_ERROR == rv.first);
+
+            return std::move(reg);
+        });
+};
+
+
+"RegressorClassic::fit rejects empty input y"_test = []
+{
+    Tsetlini::make_regressor_classic("{}")
+        .rightMap(
+        [](auto && reg)
+        {
+            std::vector<Tsetlini::aligned_vector_char> X{{1, 0, 1}, {1, 0, 0}, {0, 0, 0}};
+            Tsetlini::response_vector_type y;
+
+            auto const rv = reg.fit(X, y);
+
+            expect(that % Tsetlini::StatusCode::S_VALUE_ERROR == rv.first);
+
+            return std::move(reg);
+        });
+};
+
+
+"RegressorClassic::fit rejects input X with rows of unequal length"_test = []
+{
+    Tsetlini::make_regressor_classic("{}")
+        .rightMap(
+        [](auto && reg)
+        {
+            std::vector<Tsetlini::aligned_vector_char> X{{1, 0, 1}, {1, 0}, {0, 0, 0}};
+            Tsetlini::response_vector_type y{1, 0, 0};
+
+            auto const rv = reg.fit(X, y);
+
+            expect(that % Tsetlini::StatusCode::S_VALUE_ERROR == rv.first);
+
+            return std::move(reg);
+        });
+};
+
+
+"RegressorClassic::fit rejects input X with non-0/1 values"_test = []
+{
+    Tsetlini::make_regressor_classic("{}")
+        .rightMap(
+        [](auto && reg)
+        {
+            std::vector<Tsetlini::aligned_vector_char> X{{1, 0, 1}, {1, 0, -1}, {0, 2, 0}};
+            Tsetlini::response_vector_type y{1, 0, 0};
+
+            auto const rv = reg.fit(X, y);
+
+            expect(that % Tsetlini::StatusCode::S_VALUE_ERROR == rv.first);
+
+            return std::move(reg);
+        });
+};
+
+
+"RegressorClassic::fit rejects input X and y with unequal dimensions"_test = []
+{
+    Tsetlini::make_regressor_classic("{}")
+        .rightMap(
+        [](auto && reg)
+        {
+            std::vector<Tsetlini::aligned_vector_char> X{{1, 0, 1}, {1, 0, 0}, {0, 0, 0}};
+            Tsetlini::response_vector_type y{1, 0, 0, 1};
+
+            auto const rv = reg.fit(X, y);
+
+            expect(that % Tsetlini::StatusCode::S_VALUE_ERROR == rv.first);
+
+            return std::move(reg);
+        });
+};
+
+
+"RegressorClassic::fit rejects input y with negative response"_test = []
+{
+    Tsetlini::make_regressor_classic("{}")
+        .rightMap(
+        [](auto && reg)
+        {
+            std::vector<Tsetlini::aligned_vector_char> X{{1, 0, 1}, {1, 0, 0}, {0, 0, 0}};
+            Tsetlini::response_vector_type y{1, 0, -21};
+
+            auto const rv = reg.fit(X, y);
+
+            expect(that % Tsetlini::StatusCode::S_VALUE_ERROR == rv.first);
+
+            return std::move(reg);
+        });
+};
+
+
+"RegressorClassic::fit rejects input y with response over Threshold"_test = []
+{
+    Tsetlini::make_regressor_classic(R"({"threshold": 15})")
+        .rightMap(
+        [](auto && reg)
+        {
+            std::vector<Tsetlini::aligned_vector_char> X{{1, 0, 1}, {1, 0, 0}, {0, 0, 0}};
+            Tsetlini::response_vector_type y{1, 15 + 1, 1};
+
+            auto const rv = reg.fit(X, y);
+
+            expect(that % Tsetlini::StatusCode::S_VALUE_ERROR == rv.first);
+
+            return std::move(reg);
+        });
+};
+
+
+"RegressorClassic::fit accepts valid input y with response just below Threshold"_test = []
+{
+    Tsetlini::make_regressor_classic(R"({"threshold": 15})")
+        .rightMap(
+        [](auto && reg)
+        {
+            std::vector<Tsetlini::aligned_vector_char> X{{1, 0, 1}, {1, 0, 0}, {0, 0, 0}};
+            Tsetlini::response_vector_type y{1, 15, 1};
+
+            auto const rv = reg.fit(X, y);
+
+            expect(that % Tsetlini::StatusCode::S_OK == rv.first);
+
+            return std::move(reg);
+        });
+};
+
+
+"RegressorClassic::fit accepts valid input"_test = []
+{
+    Tsetlini::make_regressor_classic("{}")
+        .rightMap(
+        [](auto && reg)
+        {
+            std::vector<Tsetlini::aligned_vector_char> X{{1, 0, 1}, {1, 0, 0}, {0, 0, 0}};
+            Tsetlini::response_vector_type y{1, 0, 2};
+
+            auto const rv = reg.fit(X, y);
+
+            expect(that % Tsetlini::StatusCode::S_OK == rv.first);
+
+            return std::move(reg);
+        });
+};
+
+
+};
+
+int main()
+{
+    auto failed = cfg<>.run({.report_errors = true});
+
+    return failed ? EXIT_FAILURE : EXIT_SUCCESS;
+}
